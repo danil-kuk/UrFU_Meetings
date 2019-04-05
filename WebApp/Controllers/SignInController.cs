ba48d86ed2bdc33799c10c9e10ce022cb62fd5dc
Danil <danil_kuk@mail.ru>
2019-04-05 13:18:39 +0500

    Возможность добавить нового пользователя через форму регистрации

--- WebApp/Controllers/SignInController.cs
+++ WebApp/Controllers/SignInController.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.DataModels;
+using WebApp.Models.DataModels.Entities;
 
 namespace WebApp.Controllers
 {
     public class SignInController : Controller
     {
+        private readonly EFDBContext _context;
+
+        public SignInController(EFDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult SignInIndex()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            else
+            {
+                TempDataMessage("message", "danger", $"Register form datas is not valid");
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public void TempDataMessage(string key, string alert, string value)
+        {
+            try
+            {
+                TempData.Remove(key);
+                TempData.Add(key, value);
+                TempData.Add("alertType", alert);
+            }
+            catch
+            {
+                Debug.WriteLine("TempDataMessage Error");
+            }
+
         }
     }
 }
