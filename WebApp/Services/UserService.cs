e80c7921859999ab83a70c809905d78caf8f7704
Danil <danil_kuk@mail.ru>
2019-04-07 19:07:03 +0500

    Добавлен механизм регистрации и проверка почты

---
+++ WebApp/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebApp.Models.DataModels.Entities;
+
+namespace WebApp.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepositoryBase<User> _repositoryUser;
+
+        public UserService(IRepositoryBase<User> repositoryUser)
+        {
+            _repositoryUser = repositoryUser;
+        }
+
+        public void DeleteUser(User user)
+        {
+            _repositoryUser.Remove(user);
+        }
+
+        public User GetByFilter(Expression<Func<User, bool>> filter)
+        {
+            return _repositoryUser.GetByFilter(filter);
+        }
+
+        public User GetById(int id)
+        {
+            return _repositoryUser.GetById(id);
+        }
+
+        public void InsertUser(User user)
+        {
+            _repositoryUser.Insert(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            _repositoryUser.Update(user);
+        }
+    }
+}
