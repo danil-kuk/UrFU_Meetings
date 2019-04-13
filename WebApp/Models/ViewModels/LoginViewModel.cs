b09f9cff4ea9c87103da14c645715b567a40ed89
Danil <danil_kuk@mail.ru>
2019-04-14 01:57:28 +0500

    Исправление ошибки при авторизации

--- WebApp/Models/ViewModels/LoginViewModel.cs
+++ WebApp/Models/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -15,6 +16,7 @@ namespace WebApp.Models.ViewModels
 
         [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
+        [Remote("PasswordCheck", "Login", AdditionalFields = "Email", HttpMethod = "Post", ErrorMessage = "Проверьте правильность введеного пароля")]
         public string Password { get; set; }
     }
 }
