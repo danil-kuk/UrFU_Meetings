c430f658f60200c080a36807d400cc6958734d49
Danil <danil_kuk@mail.ru>
2019-05-12 19:40:34 +0500

    Добавлены обложки мероприятий

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Автоматическое переключение обложки мероприятия при изменении его темы
+$(function () {
+    $("#DropdownList").change(function () {
+        var theme = $(this);
+        var coverId = theme.val();
+        $("#eventCover").attr("src", "/img/cover" + coverId + ".svg");
+    })
+});
