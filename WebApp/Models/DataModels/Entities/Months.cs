86f42657bb4ea3bc10c2574489865a68f83bfc66
danil-kuk <danilkuk5@gmail.com>
2019-05-19 21:15:02 +0500

    Базовая верстка для доски мероприятий

---
+++ WebApp/Models/DataModels/Entities/Months.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.DataModels.Entities
+{
+    public enum Months
+    {
+        январь = 1,
+        февраль,
+        март,
+        апрель,
+        май,
+        июнь,
+        июль,
+        август,
+        сентябрь,
+        октябрь,
+        декабрь
+    }
+}
