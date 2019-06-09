d1108311270b75876676b99b071c94eda36fd115
Danil <danilkuk5@gmail.com>
2019-06-09 19:22:57 +0500

    Обновление до ASP.NET Core 2.2

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -55,7 +55,7 @@ namespace WebApp
                 {
                     options.LoginPath = new PathString("/Login");
                 });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
